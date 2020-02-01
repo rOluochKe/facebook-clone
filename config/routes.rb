ee2dcc6b504db4923f92a870f42853a910427111
ampaire <femayapr@gmail.com>
2020-01-31 18:47:48 -0600

    Configure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/show'
-  get 'users/new'
-  get 'users/show'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#start"
+
+  resources :microposts, only: [:create, :destroy, :index, :show]
+  
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+ 
 end
