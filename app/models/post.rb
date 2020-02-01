4049a0e2137b26fe72e8355a4d5f5b1c1d8e728f
ampaire <femayapr@gmail.com>
2020-01-31 18:36:07 -0600

    Add the relationship between users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
   validates :title, presence: true,  length: {maximum: 50}
   validates :description, presence: true,  length: {maximum: 50} 
   
